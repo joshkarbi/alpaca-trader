5d8fdf40681e6816f4df0bb88e60ac78f8761c60
joshualarbi <jkarbi3@uwo.ca>
2018-08-27 21:33:55 -0400

    Initial Commit

---
+++ myAlgo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myAlgo
+//
+//  Created by Joshua Karbi on 2018-08-27.
+//  Copyright © 2018 Joshua Karbi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
