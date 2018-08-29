8da1df3bcbe6293713175f22f31fad9fc5f3500b
joshualarbi <jkarbi3@uwo.ca>
2018-08-29 17:35:30 -0400

    File system updates

---
+++ Build/Intermediates/myAlgo.build/Debug/myAlgo.build/Objects-normal/x86_64/FileReadingUtilities-2ACC6B43818AC173.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/joshuakarbi/Desktop/trader/myAlgo/tools/FileReadingUtilities.cpp \
+  /Users/joshuakarbi/Desktop/trader/myAlgo/tools/FileReadingUtilities.hpp
---
+++ Build/Intermediates/myAlgo.build/Debug/myAlgo.build/Objects-normal/x86_64/Logger.d
@@ -0,0 +1,3 @@
+dependencies: /Users/joshuakarbi/Desktop/trader/myAlgo/tools/Logger.cpp \
+  /Users/joshuakarbi/Desktop/trader/myAlgo/tools/Logger.hpp \
+  /Users/joshuakarbi/Desktop/trader/myAlgo/tools/FileReadingUtilities.hpp
