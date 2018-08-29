cbad06447b8f130af91ecabf3cb6db2d85bd6d69
joshualarbi <jkarbi3@uwo.ca>
2018-08-29 14:57:27 -0400

    More changes

--- Build/Intermediates/myAlgo.build/Debug/myAlgo.build/Objects-normal/x86_64/Trader.d
+++ Build/Intermediates/myAlgo.build/Debug/myAlgo.build/Objects-normal/x86_64/Trader.d
@@ -3,23 +3,9 @@ dependencies: /Users/joshuakarbi/Desktop/trader/myAlgo/trading/Trader.cpp \
   /Users/joshuakarbi/Desktop/trader/myAlgo/trading/Stats.hpp \
   /Users/joshuakarbi/Desktop/trader/myAlgo/trading/Portfolio.hpp \
   /Users/joshuakarbi/Desktop/trader/myAlgo/trading/Holding.hpp \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/Order.hpp \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/cpr.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/api.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/auth.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/defines.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/cprtypes.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/digest.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/multipart.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/payload.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/response.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/cookies.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/error.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/session.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/body.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/max_redirects.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/parameters.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/proxies.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/timeout.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/low_speed.h \
-  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/../../../cpr/ssl_options.h
+  /Users/joshuakarbi/Desktop/trader/myAlgo/trading/Order.hpp /cpr/cpr.h \
+  /cpr/api.h /cpr/auth.h /cpr/defines.h /cpr/cprtypes.h /cpr/digest.h \
+  /cpr/multipart.h /cpr/payload.h /cpr/response.h /cpr/cookies.h \
+  /cpr/error.h /cpr/session.h /cpr/body.h /cpr/max_redirects.h \
+  /cpr/parameters.h /cpr/proxies.h /cpr/timeout.h /cpr/low_speed.h \
+  /cpr/ssl_options.h
