b1bdf35c9b97cc854604667881e68b021373e21d
joshualarbi <jkarbi3@uwo.ca>
2018-09-06 15:24:43 -0400

    Adding boost library comments to build script

--- myAlgo/build.bash
+++ myAlgo/build.bash
@@ -2,3 +2,8 @@
 # Add new .cpp files to link line below
 
 g++ -std=c++11 main.cpp trading/Order.cpp trading/Holding.cpp trading/Portfolio.cpp trading/Trader.cpp trading/Stats.cpp tools/BasicAuthorization.cpp tools/FileReadingUtilities.cpp tools/Logger.cpp tools/NetworkingUtilities.cpp -lcurl
+
+
+# For TEST files (ie. using Boost Test) 
+# include compiler flag "-I /usr/local/lib/boost_1_68_0"
+# and linker flag /usr/local/lib/boost_1_68_0/stage/lib
