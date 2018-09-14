c2aa81f1aab05485c81789229ca8453d2231d58c
joshualarbi <jkarbi3@uwo.ca>
2018-09-14 19:24:07 -0400

    Adding TODO to build script

--- myAlgo/build.bash
+++ myAlgo/build.bash
@@ -1,5 +1,6 @@
 # Run using bash build.bash
 # Add new .cpp files to link line below
+# TODO: use CMake
 
 # MAIN APPLICATION EXECUTABLE
 g++ -std=c++11 -o trader.exe main.cpp trading/Order.cpp trading/Holding.cpp trading/Portfolio.cpp trading/Trader.cpp trading/Stats.cpp tools/BasicAuthorization.cpp tools/FileReadingUtilities.cpp tools/Logger.cpp tools/NetworkingUtilities.cpp -lcurl
@@ -9,5 +10,6 @@ g++ -std=c++11 -o trader.exe main.cpp trading/Order.cpp trading/Holding.cpp trad
 # add compiler flag "-I /usr/local/lib/boost_1_68_0"
 # add linker flag "-L /usr/local/lib/boost_1_68_0/stage/lib"
 # add flag -lboost_unit_test_framework
+# RUN test files with --log_level=all argument
 
 g++ tests/TestNetworkingUtilities.cpp -std=c++11 -o TestNetworkingUtilities.exe tools/NetworkingUtilities.cpp -I /usr/local/lib/boost_1_68_0/ -L /usr/local/lib/boost_1_68_0/stage/lib/ -lboost_unit_test_framework -lcurl
