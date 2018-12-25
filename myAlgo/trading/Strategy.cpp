bc3d5c027883bcbd9eca8c559ac1b5e9a71a27c1
joshualarbi <jkarbi3@uwo.ca>
2018-12-25 16:45:58 -0500

    Reading config file for trading limits/params, storing in map

--- myAlgo/trading/Strategy.cpp
+++ myAlgo/trading/Strategy.cpp
@@ -2,6 +2,9 @@
 
 #include "Strategy.hpp"
 
+#include "FileReadingUtilities.hpp"
+#include <cstddef>
+
 namespace trading
 {
 	bool shouldBuy(const Order& buy)
@@ -20,5 +23,29 @@ namespace trading
 		// ... //
 
 		return result;
+	}
+
+	bool setup(const std::string& fileName) 
+	{
+		if (fileExists(fileName)) 
+		{
+			std::vector<std::string> fileLines = getLines(fileName);
+			for (std::string& line : fileLines) 
+			{
+				// split lines on space and insert key value
+				size_t splitIndex = line.find_first_of(" ");
+				while (splitIndex != std::string::npos)
+  				{
+    				std::string key = splitIndex.substr(0, splitIndex);
+    				std::string value = splitIndex.substr(splitIndex);
+    				parameters.insert(parameters.begin(), std::pair<std::string, std::string>(key, value));
+  				}
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
--- myAlgo/trading/Strategy.hpp
+++ myAlgo/trading/Strategy.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "Order.hpp"
+#include <map>
 
 namespace trading
 {
@@ -16,7 +17,14 @@ namespace trading
 			// @return true if sell agrees with strategy
 			bool shouldSell(const Order& sell);
 
+			// @brief fill parameters map from config file
+			// @return true on success, false on failure (ie. file does not exist)
+			// Note: expects config file to be formatted with <param><space><value> on each line
+			bool setup(const std::string& fileName);
+
 		private:
-			// TODO: add a whole bunch of parameters supported by Tradier API
+			// contains key-value pairs of trading parameters and limits
+			// ie. "buyLimit" -> "$500"
+			std::map<std::string, std::string> parameters;
 	};
 }
